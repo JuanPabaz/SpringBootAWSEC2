3f450b95ef6611c6556191b01d9684ed576c19c3
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-01-23 09:34:44 -0500

    feat: add project

---
+++ src/main/java/com/ec2/SpringBootEc2AwsApplication.java
@@ -0,0 +1,13 @@
+package com.ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEc2AwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEc2AwsApplication.class, args);
+	}
+
+}
